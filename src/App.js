73ff2f79bc3d11bc8ead7843afadbc37ab9030a0
Gökay Ay <gokayay98@gmail.com>
2021-05-16 20:12:55 +0300

    feat(master): theming without background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from "@material-ui/core/styles"
-
 import {
   BrowserRouter as Router,
   Switch, Route, Link
 } from "react-router-dom";
-
 import {
   Drawer, List, ListItem,
   ListItemIcon, ListItemText,
-  Container,
+  Container
 } from "@material-ui/core";
-
 import HomeIcon from "@material-ui/icons/Home";
 import InfoIcon from '@material-ui/icons/Info';
 import ListIcon from '@material-ui/icons/List';
 import Home from './components/Home';
 import About from './components/About';
 import CryptoList from './components/CryptoList';
+import {
+  orange,
+  lightBlue,
+  deepPurple,
+  deepOrange
+} from "@material-ui/core/colors";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import {Switch as Toggle} from '@material-ui/core';
+ 
+
 
 const useStyles = makeStyles((theme) => ({
   drawerPaper: { width: 'inherit' },
@@ -29,7 +36,28 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
+  const [darkState, setDarkState] = useState(false);
+  const palletType = darkState ? "dark" : "light";
+  const mainPrimaryColor = darkState ? orange[500] : lightBlue[500];
+  const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500];
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+      primary: {
+        main: mainPrimaryColor
+      },
+      secondary: {
+        main: mainSecondaryColor
+      }
+    }
+  });
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  };
+
+
   return (
+  <ThemeProvider theme={darkTheme}>
     <Router>
       <div style={{ display: 'flex' }}>
         <Drawer
@@ -65,6 +93,12 @@ function App() {
               </ListItem>
             </Link>
           </List>
+          <br></br>
+          <br></br>
+          <div style={{'margin-left': '1rem'}}>
+            Change Theme
+          <Toggle checked={darkState} onChange={handleThemeChange} />
+          </div>
         </Drawer>
         <Switch>
           <Route exact path="/">
@@ -85,6 +119,7 @@ function App() {
         </Switch>
       </div>
     </Router>
+  </ThemeProvider> 
   );
 }
 
